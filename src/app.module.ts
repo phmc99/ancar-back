c1cbd435bec8cd7ba8f5803ccddbf1ff509b9a0e
Pedro Costa <phmc99@outlook.com>
2023-01-06 19:51:47 -0300

    feat: database related

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
